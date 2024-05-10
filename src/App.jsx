2ebb5bffb29862fc89ca255d51359020b083f9d2
JeevaA1225 <jeevamurugan111@gmail.com>
2024-05-10 10:59:24 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import './index.css';
+import WeatherApp from './components/WeatherApp';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+      {/* <Anime /> */}
+    </div>
+  );
+}
+
+export default App;
